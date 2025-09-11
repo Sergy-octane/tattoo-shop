9677a0265d62a13fd1870f1838ad6046460a6b23
Sergy Ferreira <sergyferreira153@gmail.com>
2025-09-10 20:28:36 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,17 +12,28 @@ function App() {
         </div>
 
         {/* Texto curvado */}
-        <svg className="w-full max-w-4xl h-auto relative" viewBox="0 0 800 200">
-          <defs>
-            <path id="curve" d="M50 150 Q400 -50 750 150" />
-          </defs>
-          <text fontSize="35" fontFamily="'UnifrakturCook', cursive">
-            <textPath href="#curve" startOffset="50%" textAnchor="middle">
-              <tspan fill="white">LA DOL </tspan>
-              <tspan fill="black">OROSA</tspan>
-            </textPath>
-          </text>
-        </svg>
+        <svg className="w-full max-w-4xl h-auto" viewBox="0 0 800 150">
+    <rect x="0" y="0" width="400" height="100" fill="black" />
+    <rect x="400" y="0" width="400" height="100" fill="white" />
+    <defs>
+    <path id="curve" d="M50 150 Q400 -50 750 200" />
+    </defs>
+    <text fontSize="35" fontFamily="'UnifrakturCook', cursive">
+    <textPath href="#curve" startOffset="50%" textAnchor="middle">
+      <tspan fill="white">L A D O L </tspan>
+      <tspan fill="black">O R O S A</tspan>
+      </textPath>
+      </text>
+      {/* Texto debajo */}
+  <text x="50%" y="80" fontSize="10" fontFamily="'UnifrakturCook', cursive" textAnchor="middle" fill="gray">
+    C L A N 
+  </text>
+
+  <text x="50%" y="95" fontSize="16" fontFamily="'UnifrakturCook', cursive" textAnchor="middle" fill="gray">
+    1 .  3 . 1 . 2
+  </text>
+      </svg>
+
       </div>
 
       {/* CONTENIDO */}
